5b306a5510209d48b887865fbbe03fdc636ba9e2
Timmeh42 <tmeier42+github@gmail.com>
2020-12-02 09:18:18 +0200

    day 2

---
+++ day 2/part2.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const rawInput = fs.readFileSync('input.txt', 'utf8').trimEnd();
+
+const input = rawInput.split('\n');
+
+console.log(
+    input.reduce((n, line) => {
+        let [_, min, max, char, password] = line.match(/(\d+)-(\d+) (\w): (\w+)/i).map((s, i) => i === 1 || i === 2 ? parseInt(s) : s);
+        if (password[min-1] === char ^ password[max-1] === char) {
+            console.log(min, max, char, password);
+            return n+1;
+        } else {
+            return n;
+        }
+    }, 0)
+);
