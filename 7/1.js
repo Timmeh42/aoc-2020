f3834337182e4092d1d6daa967103a1e08889f4a
Timmeh42 <tmeier42+github@gmail.com>
2020-12-07 09:33:37 +0200

    day 7

---
+++ 7/1.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const rawInput = fs.readFileSync('input.txt', 'utf8').trimEnd();
+const input = rawInput.split('\n');
+
+const bags = new Map();
+for (let bagString of input) {
+    const bagName = bagString.match(/^\w+ \w+(?= bag)/g)[0];
+    const innerBags = bagString.match(/\d+ \w+ \w+(?= bag)/g) || [];
+    const newBag = {
+        name: bagName,
+        holds: innerBags.map(inn => inn.slice(2)),
+        heldby: new Set(),
+        counted: false,
+    }
+    bags.set(bagName, newBag);
+}
+
+bags.forEach(bag => bag.holds.forEach(name => bags.get(name).heldby.add(bag)))
+
+
+let count = 0;
+
+let allParents = [];
+const shiny_gold = bags.get('shiny gold');
+const holders = [...shiny_gold.heldby.values()].filter(b => !b.counted)
+allParents.push(...holders);
+shiny_gold.counted = true;
+
+while (allParents.length) {
+    let newParents = [];
+    console.log('--');
+    for (let i = allParents.length - 1; i >= 0; i--) {
+        let bag = allParents[i];
+        if (!bag.counted) {
+            console.log(bag.name, bag.counted)
+            let holders = [...bag.heldby.values()].filter(b => !b.counted);
+            newParents.push(...holders);
+            bag.counted = true;
+            count ++;
+        }
+    }
+    allParents = newParents;
+}
+console.log(count)
---
+++ 7/2.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const rawInput = fs.readFileSync('input.txt', 'utf8').trimEnd();
+const input = rawInput.split('\n');
+
+const bags = new Map();
+for (let bagString of input) {
+    const bagName = bagString.match(/^\w+ \w+(?= bag)/g)[0];
+    const innerBags = bagString.match(/\d+ \w+ \w+(?= bag)/g) || [];
+    const newBag = {
+        name: bagName,
+        holds: innerBags.map(inn => [parseInt(inn[0]), inn.slice(2)]),
+        count: null,
+    }
+    bags.set(bagName, newBag);
+}
+
+function countBag (name) {
+    const bag = bags.get(name);
+    if (bag.count) return bag.count;
+    console.log(bag)
+    bag.count = bag.holds.reduce((c, b) => c + b[0] * countBag(b[1]), 1);
+    return bag.count;
+}
+
+console.log(countBag('shiny gold') - 1);
