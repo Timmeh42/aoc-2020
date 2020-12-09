0cea070b4d39e67cd9b115f39ffc6d8b4b03d24b
Tim <tmeier42+github@gmail.com>
2020-12-09 20:54:45 +0200

    fix missing day 1 and day 2

--- 1/2.js
+++ 1/2.js
@@ -1,3 +1,11 @@
 const fs = require('fs');
 const rawInput = fs.readFileSync('input.txt', 'utf8').trimEnd();
-const input = rawInput.split('\n');
+const input = rawInput.split('\n').map(l => parseInt(l));
+
+for (let i = input.length - 1; i >= 0; i--)
+for (let j = i - 1; j >= 0; j--)
+for (let k = j - 1; k >= 0; k--) {
+    if (input[i] + input[j] + input[k] === 2020) {
+        console.log(input[i] * input[j] * input[k]);
+    }
+}
