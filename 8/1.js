e5e88f9ce01617ee29403a6a25f47270ff0738d7
Tim <tmeier42+github@gmail.com>
2020-12-09 21:39:03 +0200

    modulise day 8

--- 8/1.js
+++ 8/1.js
@@ -1,27 +1,25 @@
-const fs = require('fs');
-const rawInput = fs.readFileSync('input.txt', 'utf8').trimEnd();
-const input = rawInput.split('\n');
+module.exports = function (input) {
+    let acc = 0;
+    let pnt = 0;
 
-let acc = 0;
-let pnt = 0;
+    while (true) {
+        const instr = input[pnt];
+        if (!instr) break;
+        input[pnt] = null;
+        const code = instr.slice(0, 3);
+        const num = parseInt(instr.slice(4));
 
-while (true) {
-    const instr = input[pnt];
-    if (!instr) break;
-    input[pnt] = null;
-    const code = instr.slice(0, 3);
-    const num = parseInt(instr.slice(4));
-
-    switch (code) {
-        case 'jmp': {
-            pnt += num - 1;
-            break;
+        switch (code) {
+            case 'jmp': {
+                pnt += num - 1;
+                break;
+            }
+            case 'acc': {
+                acc += num;
+                break;
+            }
         }
-        case 'acc': {
-            acc += num;
-            break;
-        }
+        pnt += 1;
     }
-    pnt += 1;
+    return acc;
 }
-console.log(pnt, acc);
--- 8/2.js
+++ 8/2.js
@@ -1,77 +1,17 @@
-const fs = require('fs');
-const rawInput = fs.readFileSync('input.txt', 'utf8').trimEnd();
-let input = rawInput.split('\n');
+module.exports = function (input) {
+    let acc = 0;
+    let pnt = 0;
 
-let acc = 0;
-let pnt = 0;
+    let ran = [];
+    let tested = [];
 
-let ran = [];
-let tested = [];
-
-while (true) {
-    const instr = input[pnt];
-    if (ran.includes(pnt)) {
-        console.log('Program failed');
-        break;
-    }
-    if (pnt === input.length) {
-        console.log('Program ended');
-        break;
-    }
-    const code = instr.slice(0, 3);
-    const num = parseInt(instr.slice(4));
-
-    ran.push(pnt);
-    switch (code) {
-        case 'jmp': {
-            pnt += num - 1;
-            break;
-        }
-        case 'acc': {
-            acc += num;
-            break;
-        }
-        case 'nop': {
-            break;
-        }
-    }
-    pnt += 1;
-}
-
-outer: while (true) {
-    input = rawInput.split('\n');
-    if (tested.length === ran.length) {
-        console.log('Replacement failed');
-        break;
-    }
-    for (let i = ran.length - 1; i>= 0; i--) {
-        let p = ran[i];
-        if (!tested.includes(p)) {
-            tested.push(p);
-            if (input[p].slice(0, 3) === 'jmp') {
-                input[p] = 'nop' + input[p].slice(3);
-                console.log('changed', p);
-                break;
-            } else if (input[p].slice(0, 3) === 'nop') {
-                input[p] = 'jmp' + input[p].slice(3);
-                console.log('changed', p);
-                break;
-            }
-        }
-    }
-
-    pnt = 0;
-    acc = 0;
-    ran = [];
     while (true) {
         const instr = input[pnt];
         if (ran.includes(pnt)) {
-            console.log('Program failed', pnt);
             break;
         }
         if (pnt === input.length) {
-            console.log('Program ended');
-            break outer;
+            break;
         }
         const code = instr.slice(0, 3);
         const num = parseInt(instr.slice(4));
@@ -93,6 +33,57 @@ outer: while (true) {
         pnt += 1;
     }
 
+    outer: while (true) {
+        let inp = [...input];
+        if (tested.length === ran.length) {
+            break;
+        }
+        for (let i = ran.length - 1; i>= 0; i--) {
+            let p = ran[i];
+            if (!tested.includes(p)) {
+                tested.push(p);
+                if (inp[p].slice(0, 3) === 'jmp') {
+                    inp[p] = 'nop' + inp[p].slice(3);
+                    break;
+                } else if (inp[p].slice(0, 3) === 'nop') {
+                    inp[p] = 'jmp' + inp[p].slice(3);
+                    break;
+                }
+            }
+        }
 
+        pnt = 0;
+        acc = 0;
+        ran = [];
+        while (true) {
+            const instr = inp[pnt];
+            if (ran.includes(pnt)) {
+                break;
+            }
+            if (pnt === inp.length) {
+                break outer;
+            }
+            const code = instr.slice(0, 3);
+            const num = parseInt(instr.slice(4));
+
+            ran.push(pnt);
+            switch (code) {
+                case 'jmp': {
+                    pnt += num - 1;
+                    break;
+                }
+                case 'acc': {
+                    acc += num;
+                    break;
+                }
+                case 'nop': {
+                    break;
+                }
+            }
+            pnt += 1;
+        }
+
+
+    }
+    return acc;
 }
-console.log(acc);
