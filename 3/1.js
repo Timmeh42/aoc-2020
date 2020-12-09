dfa68247f178d9410c5a764c9ab316dc8fef4052
Tim <tmeier42+github@gmail.com>
2020-12-09 21:04:58 +0200

    modulise day 3

--- 3/1.js
+++ 3/1.js
@@ -1,13 +1,11 @@
-const fs = require('fs');
-const rawInput = fs.readFileSync('input.txt', 'utf8').trimEnd();
-const input = rawInput.split('\n');
-
-const width = input[0].length;
-
-let x = 0;
-let count = 0;
-for (let y = 0; y < input.length; y++) {
-    count += input[y][x] === '#';
-    x = (x+3) % width;
+module.exports = function (input) {
+    const width = input[0].length;
+    
+    let x = 0;
+    let count = 0;
+    for (let y = 0; y < input.length; y++) {
+        count += input[y][x] === '#';
+        x = (x+3) % width;
+    }
+    return count;
 }
-console.log(count);
--- 3/2.js
+++ 3/2.js
@@ -1,17 +1,15 @@
-const fs = require('fs');
-const rawInput = fs.readFileSync('input.txt', 'utf8').trimEnd();
-const input = rawInput.split('\n');
+module.exports = function (input) {
+    const width = input[0].length;
 
-const width = input[0].length;
-
-let mult = 1;
-for (let slope of [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]) {
-    let x = 0;
-    let count = 0;
-    for (let y = 0; y < input.length; y += slope[1]) {
-        count += input[y][x] === '#';
-        x = (x + slope[0]) % width;
+    let mult = 1;
+    for (let slope of [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]) {
+        let x = 0;
+        let count = 0;
+        for (let y = 0; y < input.length; y += slope[1]) {
+            count += input[y][x] === '#';
+            x = (x + slope[0]) % width;
+        }
+        mult *= count;
     }
-    mult *= count;
+    return mult;
 }
-console.log(mult);
