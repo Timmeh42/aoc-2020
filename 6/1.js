2348ee61801c57ae79ac0eef25882ac5bc3de53b
Tim <tmeier42+github@gmail.com>
2020-12-09 21:26:14 +0200

    modulise day 6

--- 6/1.js
+++ 6/1.js
@@ -1,5 +1,4 @@
-const fs = require('fs');
-const rawInput = fs.readFileSync('input.txt', 'utf8').trimEnd();
-const groups = rawInput.split('\n\n');
-
-console.log(groups.reduce((sum, group) => sum + new Set(group.replace(/\n/g, '').split('')).size, 0));
+module.exports = function (input) {
+    input = input.join('\n').split('\n\n');
+    return input.reduce((sum, group) => sum + new Set(group.replace(/\n/g, '').split('')).size, 0);
+}
--- 6/2.js
+++ 6/2.js
@@ -1,19 +1,17 @@
-const fs = require('fs');
-const rawInput = fs.readFileSync('input.txt', 'utf8').trimEnd();
-const groups = rawInput.split('\n\n');
-
-let count = 0;
-for (let group of groups) {
-    let lines = group.split('\n');
-    alph: for (let c of 'abcdefghijklmnopqrstuvwxyz'.split('')) {
-        for (let line of lines) {
-            if (!line.split('').includes(c)) {
-                continue alph;
+module.exports = function (input) {
+    input = input.join('\n').split('\n\n');
+    let count = 0;
+    for (let group of input) {
+        let lines = group.split('\n');
+        alph: for (let c of 'abcdefghijklmnopqrstuvwxyz'.split('')) {
+            for (let line of lines) {
+                if (!line.split('').includes(c)) {
+                    continue alph;
+                }
             }
+            count++;
         }
-        count++;
+        
     }
-    
+    return count;
 }
-
-console.log(count);
