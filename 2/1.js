1dca13709b7122761a72f74012719f5485031d26
Tim <tmeier42+github@gmail.com>
2020-12-09 21:00:01 +0200

    modulise day 2

--- 2/1.js
+++ 2/1.js
@@ -1,15 +1,11 @@
-const fs = require('fs');
-const rawInput = fs.readFileSync('input.txt', 'utf8').trimEnd();
-const input = rawInput.split('\n');
-
-console.log(
-    input.reduce((n, line) => {
-        let [_, min, max, char, password] = line.match(/(\d+)-(\d+) (\w): (\w+)/i).map((s, i) => i === 1 || i === 2 ? parseInt(s) : s);
-        let count = (password.match(new RegExp(char, 'g')) || []).length;
-        if (min <= count && max >= count) {
-            return n+1;
-        } else {
-            return n;
-        }
-    }, 0)
-);
+module.exports = function (input) {
+    return input.reduce((n, line) => {
+            let [_, min, max, char, password] = line.match(/(\d+)-(\d+) (\w): (\w+)/i).map((s, i) => i === 1 || i === 2 ? parseInt(s) : s);
+            let count = (password.match(new RegExp(char, 'g')) || []).length;
+            if (min <= count && max >= count) {
+                return n+1;
+            } else {
+                return n;
+            }
+        }, 0);    
+}
