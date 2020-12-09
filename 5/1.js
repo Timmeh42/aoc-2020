e927abf0f2c96d98013c8b7c24787d194ba60bb3
Tim <tmeier42+github@gmail.com>
2020-12-09 21:21:34 +0200

    modulise day 5

--- 5/1.js
+++ 5/1.js
@@ -1,7 +1,3 @@
-const fs = require('fs');
-const rawInput = fs.readFileSync('input.txt', 'utf8').trimEnd();
-const input = rawInput.split('\n');
-
-console.log(
-    input.reduce((max, b) => Math.max(max, parseInt(b.replace(/\w/g, match => ({F: '0', B: '1', L:'0', R: '1'}[match])), 2)), -Infinity)
-);
+module.exports = function (input) {
+    return input.reduce((max, b) => Math.max(max, parseInt(b.replace(/\w/g, match => ({F: '0', B: '1', L:'0', R: '1'}[match])), 2)), -Infinity);
+}
--- 5/2.js
+++ 5/2.js
@@ -1,11 +1,7 @@
-const fs = require('fs');
-const rawInput = fs.readFileSync('input.txt', 'utf8').trimEnd();
-
-const seat = rawInput
-                .split('\n')
-                .map(b => parseInt(b.replace(/F|L/g, '0').replace(/B|R/g, '1'), 2))
-                .sort((a, b) => a - b)
-                .find((v, i, s) => s[i-1] === v-2)
-                - 1
-            ;
-console.log(seat);
+module.exports = function (input) {
+    return input.map(b => parseInt(b.replace(/F|L/g, '0').replace(/B|R/g, '1'), 2))
+            .sort((a, b) => a - b)
+            .find((v, i, s) => s[i-1] === v-2)
+            - 1
+        ;
+}
