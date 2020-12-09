1b3c6a65071e37a8cc69b298e59db9de11c2789b
Timmeh42 <tmeier42+github@gmail.com>
2020-12-09 17:38:16 +0200

    convert day 9 to runnable exports

--- 9/1.js
+++ 9/1.js
@@ -1,22 +1,20 @@
-const fs = require('fs');
-const rawInput = fs.readFileSync('input.txt', 'utf8').trimEnd();
-const input = rawInput.split('\n').map(i => parseInt(i));
-
-let preamble = 25;
-const adds = new Set();
-
-for (let m = 0; m < input.length; m++) {
-    const n = input[m]
-    if (preamble !== 0) {
-        adds.add(n);
-        preamble--;
-    } else {
-        if ([...adds.values()].find(a => adds.has(n - a))) {
+module.exports = function (input) {
+    input = input.map(i => parseInt(i));
+    let preamble = 25;
+    const adds = new Set();
+    
+    for (let m = 0; m < input.length; m++) {
+        const n = input[m]
+        if (preamble !== 0) {
             adds.add(n);
-            adds.delete(adds.values().next().value);
+            preamble--;
         } else {
-            console.log(n);
-            break;
+            if ([...adds.values()].find(a => adds.has(n - a))) {
+                adds.add(n);
+                adds.delete(adds.values().next().value);
+            } else {
+                return n;
+            }
         }
     }
 }
--- 9/2.js
+++ 9/2.js
@@ -1,44 +1,43 @@
-const fs = require('fs');
-const rawInput = fs.readFileSync('input.txt', 'utf8').trimEnd();
-const input = rawInput.split('\n').map(i => parseInt(i));
+module.exports = function (input) {
+    input = input.map(i => parseInt(i));
+    let preamble = 25;
+    const adds = new Set();
 
-let preamble = 25;
-const adds = new Set();
+    let invalid;
 
-let invalid;
-
-for (let m = 0; m < input.length; m++) {
-    const n = input[m]
-    if (preamble !== 0) {
-        adds.add(n);
-        preamble--;
-    } else {
-        if ([...adds.values()].find(a => adds.has(n - a))) {
-            adds.add(n);
-            adds.delete(adds.values().next().value);
+    for (let m = 0; m < input.length; m++) {
+        const n = input[m]
+        if (preamble === 0) {
+            if ([...adds.values()].find(a => adds.has(n - a))) {
+                adds.add(n);
+                adds.delete(adds.values().next().value);
+            } else {
+                invalid = n;
+                break;
+            }
         } else {
-            invalid = n;
+            adds.add(n);
+            preamble--;
+        }
+    }
+
+    let start = 0;
+    let end = 0;
+    let rangeSum = 0;
+
+    while (rangeSum !== invalid) {
+        if (rangeSum < invalid || end-start <= 2) {
+            rangeSum += input[end];
+            end++;
+        }
+        if (rangeSum > invalid && end-start > 2) {
+            rangeSum -= input[start];
+            start++;
+        }
+        if (end >= input.length) {
+            console.log('failed');
             break;
         }
     }
+    return Math.max(...input.slice(start, end)) + Math.min(...input.slice(start, end));
 }
-
-let start = 0;
-let end = 0;
-let rangeSum = 0;
-
-while (rangeSum !== invalid) {
-    if (rangeSum < invalid) {
-        rangeSum += input[end];
-        end++;
-    }
-    if (rangeSum > invalid && end-start > 2) {
-        rangeSum -= input[start];
-        start++;
-    }
-    if (end >= input.length) {
-        console.log('failed');
-        break;
-    }
-}
-console.log(Math.max(...input.slice(start, end)) + Math.min(...input.slice(start, end)))
