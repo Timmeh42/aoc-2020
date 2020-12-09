399c451059be7ea4c0cfd79f3de542878ccf7fe6
Tim <tmeier42+github@gmail.com>
2020-12-09 20:57:36 +0200

    modulise day 1

--- 1/1.js
+++ 1/1.js
@@ -1,10 +1,9 @@
-const fs = require('fs');
-const rawInput = fs.readFileSync('input.txt', 'utf8').trimEnd();
-const input = rawInput.split('\n').map(l => parseInt(l));
-
-for (let i = input.length - 1; i >= 0; i--)
-for (let j = i - 1; j >= 0; j--) {
-    if (input[i] + input[j] === 2020) {
-        console.log(input[i] * input[j]);
+module.exports = function (input) {
+    input = input.map(i => parseInt(i));
+    for (let i = input.length - 1; i >= 0; i--)
+    for (let j = i - 1; j >= 0; j--) {
+        if (input[i] + input[j] === 2020) {
+            return input[i] * input[j];
+        }
     }
 }
--- 1/2.js
+++ 1/2.js
@@ -1,11 +1,10 @@
-const fs = require('fs');
-const rawInput = fs.readFileSync('input.txt', 'utf8').trimEnd();
-const input = rawInput.split('\n').map(l => parseInt(l));
-
-for (let i = input.length - 1; i >= 0; i--)
-for (let j = i - 1; j >= 0; j--)
-for (let k = j - 1; k >= 0; k--) {
-    if (input[i] + input[j] + input[k] === 2020) {
-        console.log(input[i] * input[j] * input[k]);
+module.exports = function (input) {
+    input = input.map(i => parseInt(i));
+    for (let i = input.length - 1; i >= 0; i--)
+    for (let j = i - 1; j >= 0; j--)
+    for (let k = j - 1; k >= 0; k--) {
+        if (input[i] + input[j] + input[k] === 2020) {
+            return input[i] * input[j] * input[k];
+        }
     }
 }
