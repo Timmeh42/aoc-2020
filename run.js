4e0be5d24134c91968b0ddbda1e5ee14cddbfbe4
Timmeh42 <tmeier42+github@gmail.com>
2020-12-09 17:34:24 +0200

    make runner

---
+++ run.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const argsRaw = process.argv.slice(2).join(' ');
+const argsMatch = argsRaw.match(/^(?<day>\d+)(?: (?<part>(?:1|2)\b))?(?: (?<runs>\d+)?)?/);
+if (argsMatch === null || !argsMatch.groups.day) {
+    console.error('Usage: node run <day> [<part>] [<runs>]');
+    console.error('Missing <day> input');
+    process.exit(1);
+}
+
+const {day, part, runs = 1} = argsMatch.groups;
+
+
+let input;
+try {
+    input = fs.readFileSync(`${day}/input.txt`, 'utf8').trimEnd().split('\n');
+} catch (e) {
+    console.error(`${day}/input.txt not found.`)
+    process.exit(1);
+}
+
+const parts = part ? [part] : ['1', '2'];
+for (let p of parts) {
+    let answer;
+    try {
+        answer = require(`./${day}/${p}.js`);
+    } catch (e) {
+        console.error(`${day}/${p} not found.`)
+        process.exit(1);
+    }
+    let solution;
+    const startTime = process.hrtime.bigint();
+    for (let i = 0; i < runs; i++) {
+        solution = answer(input);
+    }
+    const endTime = process.hrtime.bigint();
+    const avgTime = Number(endTime - startTime) / 1e9 / runs;
+    console.log(`Part ${p}: ${avgTime}s`);
+    console.log(solution);
+}
