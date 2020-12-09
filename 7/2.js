172c9540620248fc504c89ecd6496037de53bd1f
Tim <tmeier42+github@gmail.com>
2020-12-09 21:28:49 +0200

    modulise day 7

--- 7/2.js
+++ 7/2.js
@@ -1,24 +1,22 @@
-const fs = require('fs');
-const rawInput = fs.readFileSync('input.txt', 'utf8').trimEnd();
-const input = rawInput.split('\n');
-
-const bags = new Map();
-for (let bagString of input) {
-    const bagName = bagString.match(/^\w+ \w+(?= bag)/g)[0];
-    const innerBags = bagString.match(/\d+ \w+ \w+(?= bag)/g) || [];
-    const newBag = {
-        name: bagName,
-        holds: innerBags.map(inn => [parseInt(inn[0]), inn.slice(2)]),
-        count: null,
+module.exports = function (input) {
+    const bags = new Map();
+    for (let bagString of input) {
+        const bagName = bagString.match(/^\w+ \w+(?= bag)/g)[0];
+        const innerBags = bagString.match(/\d+ \w+ \w+(?= bag)/g) || [];
+        const newBag = {
+            name: bagName,
+            holds: innerBags.map(inn => [parseInt(inn[0]), inn.slice(2)]),
+            count: null,
+        }
+        bags.set(bagName, newBag);
     }
-    bags.set(bagName, newBag);
-}
 
-function countBag (name) {
-    const bag = bags.get(name);
-    if (bag.count) return bag.count;
-    bag.count = bag.holds.reduce((c, b) => c + b[0] * countBag(b[1]), 1);
-    return bag.count;
-}
+    function countBag (name) {
+        const bag = bags.get(name);
+        if (bag.count) return bag.count;
+        bag.count = bag.holds.reduce((c, b) => c + b[0] * countBag(b[1]), 1);
+        return bag.count;
+    }
 
-console.log(countBag('shiny gold') - 1);
+    return countBag('shiny gold') - 1;
+}
