38e595ebb0a0537b35d384d32527fcfa03e02a8a
Tim <tmeier42+github@gmail.com>
2020-12-15 08:53:40 +0200

    day 15

---
+++ 15/1.js
@@ -0,0 +1,15 @@
+module.exports = function (input) {
+    const nums = new Map(input.split(',').map((n, i) => [parseInt(n), i+1]));
+    let last = 0;
+    let next = 0;
+    for (let i = nums.size + 1; i <= 2020; i++) {
+        last = next;
+        if (nums.has(last)) {
+            next = i - nums.get(last);
+        } else {
+            next = 0;
+        }
+        nums.set(last, i);
+    }
+    return last;
+}
