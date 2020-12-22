1cf81be30756691eee6bd47b1c0ccaee1b74b965
Tim <tmeier42+github@gmail.com>
2020-12-22 13:10:09 +0200

    faster hash function, neaten things up

--- 22/2.js
+++ 22/2.js
@@ -1,36 +1,38 @@
 module.exports = function (input) {
     let decks = input.match(/(\n\d+)+/g).map(s => s.trim().split('\n').map(n => parseInt(n)).reverse());
-    return combat(decks[0], decks[1])[1];
+    return combat(decks[0], decks[1], true);
 }
 
-function combat (deck1, deck2) {
-    let memory = new Set();
-    let winner = null;
-    while (deck1.length && deck2.length) {
-        let rwinner = false;
-        let hash = deck1.join(' ') + '  ' + deck2.join(' ');
+function combat (deck1, deck2, orig = false) {
+    const memory = new Set();
+    let gameWinner = null;
+    while (deck1.length !== 0 && deck2.length !== 0) {
+        let roundWinner = false;
+        const hash = deck1.reduce((sum, c, i) => sum + c * (i + 1), 0) + 100000 * deck2.reduce((sum, c, i) => sum + c * (i + 1), 0);
         if (memory.has(hash)) {
-            winner = false;
+            gameWinner = false;
             break;
         }
-        let c1 = deck1.pop(),
+        memory.add(hash);
+        const c1 = deck1.pop(),
             c2 = deck2.pop();
         if (c1 <= deck1.length && c2 <= deck2.length) {
-            [rwinner] = combat(deck1.slice(-c1), deck2.slice(-c2));
+            roundWinner = combat(deck1.slice(-c1), deck2.slice(-c2));
         } else {
-            rwinner = c2 > c1;
+            roundWinner = c2 > c1;
         }
-        memory.add(hash);
-        if (rwinner) {
+        if (roundWinner) {
             deck2.unshift(c1, c2);
         } else {
             deck1.unshift(c2, c1);
         }
     }
-    if (winner === null) {
-        winner = deck1.length === 0
+    if (gameWinner === null) {
+        gameWinner = deck1.length === 0;
     };
-    let windeck = winner ? deck2 : deck1;
-    let score = windeck.reduce((sum, c, i) => sum + c * (i + 1), 0);
-    return [winner, score];
+    if (orig) {
+        return (gameWinner ? deck2 : deck1).reduce((sum, c, i) => sum + c * (i + 1), 0);
+    } else {
+        return gameWinner;
+    }
 }
