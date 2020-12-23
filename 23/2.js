6b7779e7029565d888f7a0d0e136663c35accca6
Tim <tmeier42+github@gmail.com>
2020-12-23 11:54:31 +0200

    use a simpler linked list

--- 23/2.js
+++ 23/2.js
@@ -1,43 +1,29 @@
 module.exports = function (input) {
     let cups = Array(1_000_000);
     for (let j = 1_000_000; j >= 10; j--) {
-        const cup = {
-            id: j,
-            next: cups[j+1],
-        }
-        cups[j] = cup;
+        cups[j] = j+1;
     }
     for (let i = input.length - 1; i >= 0; i--) {
         const n = parseInt(input[i]);
         const m = parseInt(input[(i+1) % input.length]);
-        const cup = {
-            id: n,
-            next: cups[m],
-        }
-        cups[n] = cup;
+        cups[n] = m;
     }
-    const lastCup = cups[parseInt(input[input.length-1])];
-    lastCup.next = cups[10];
-    let currentCup = cups[parseInt(input[0])];
-    cups[1_000_000].next = currentCup;
+    cups[parseInt(input[input.length-1])] = 10;
+    let currentCup = parseInt(input[0]);
+    cups[1_000_000] = currentCup;
 
     for (let round = 1; round <= 10_000_000; round++) {
-        let takeOut = currentCup.next;
-        currentCup.next = currentCup.next.next.next.next
-        let destinationCup;
-        for (let d = currentCup.id-1; true; d--) {
-            if (d === 0) d = 1_000_000;
-            if (d === takeOut.id || d === takeOut.next.id || d === takeOut.next.next.id) continue;
-            destinationCup = cups[d];
-            break;
+        let takeOut = cups[currentCup];
+        cups[currentCup] = cups[cups[cups[cups[currentCup]]]];
+        let destinationCup = currentCup;
+        while (true) {
+            destinationCup--;
+            if (destinationCup === 0) destinationCup = 1_000_000;
+            if (destinationCup !== takeOut && destinationCup !== cups[takeOut] && destinationCup !== cups[cups[takeOut]]) break;
         }
-        takeOut.next.next.next = destinationCup.next;
-        destinationCup.next = takeOut;
-        currentCup = currentCup.next;
+        cups[cups[cups[takeOut]]] = cups[destinationCup];
+        cups[destinationCup] = takeOut;
+        currentCup = cups[currentCup];
     }
-    return printCups(cups[1]);
-}
-
-function printCups (currentCup) {
-    return currentCup.next.id * currentCup.next.next.id;
+    return cups[1] * cups[cups[1]];
 }
