672c6cf45d8f2613074ef10fcb4c1856627936e4
Tim <tmeier42+github@gmail.com>
2020-12-25 09:54:44 +0200

    day 25

---
+++ 25/1.js
@@ -0,0 +1,35 @@
+module.exports = function (input) {
+    let [doorKey, cardKey] = input.split('\n').map(n => parseInt(n));
+    let doorloops = 0;
+    let doorReplica = 1;
+    while (++doorloops) {
+        doorReplica *= 7;
+        doorReplica %= 20201227;
+        if (doorReplica === doorKey) {
+            break;
+        }
+    }
+
+    let cardloops = 0;
+    let cardReplica = 1;
+    while (++cardloops) {
+        cardReplica *= 7;
+        cardReplica %= 20201227;
+        if (cardReplica === cardKey) {
+            break;
+        }
+    }
+
+    let secret = 1;
+    for (let i = 0; i < cardloops; i++) {
+        secret *= doorReplica;
+        secret %= 20201227;
+    }
+
+    let secret2 = 1;
+    for (let i = 0; i < doorloops; i++) {
+        secret2 *= cardReplica;
+        secret2 %= 20201227;
+    }
+    return [secret, secret2];
+}
